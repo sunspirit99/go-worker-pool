bf8c5fbd949c6c9e7762aae3a59313d93caca660
sunspirit9999 <phamnhatlinh19999@gmail.com>
2022-12-26 15:58:06 +0700

    add result for worker

--- example/ex1.go
+++ example/ex1.go
@@ -1,8 +1,8 @@
 package example
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
 
 	"github.com/sunspirit9999/go-worker-pool/workerpool"
 )
@@ -18,24 +18,79 @@ func New() {
 
 	fmt.Println("Total workers : ", pool.TotalWorkers())
 
-	var task []*workerpool.Task
+	task1 := workerpool.NewTask("Sum", func() interface{} {
+		res := Sum(1, 2)
+		return res
+	})
 
+	var tasks []*workerpool.Task
 	for i := 1; i <= numOfTasks; i++ {
-		task = append(task, workerpool.NewTask("Sum", func() {
-			a := rand.Intn(10)
-			b := rand.Intn(10)
-			Sum(a, b)
-		}))
+		task2 := workerpool.NewTask(fmt.Sprintf("Divide_%d", i), func() interface{} {
+			mul, div, err := MultipleAndDivide(1, 1)
+			if err != nil {
+				return err
+			}
+			return []int{mul, div}
+		})
+		tasks = append(tasks, task2)
 	}
 
-	pool.AssignTask(task...)
+	pool.AssignTask(append(tasks, task1)...)
+
+	// Get result from the pool
+	result := pool.GetResult("Divide_10")
+	switch res := result.(type) {
+	case error:
+		fmt.Printf("type : %T, response : %+v\n", res, res)
+	case []int:
+		fmt.Printf("type : %T, response : %+v\n", res, res)
+	}
+
+	// pool.RefreshWorker(1, 2, 3)
+
+	for _, worker := range pool.Workers {
+		fmt.Printf("Worker %d : %+v\n", worker.Id, worker.Result)
+	}
+
+	pool.AddWorker(-4)
+
+	for _, worker := range pool.Workers {
+		fmt.Printf("Worker %d : %+v\n", worker.Id, worker.Result)
+	}
+
+	pool.AddWorker(6)
+
+	for _, worker := range pool.Workers {
+		fmt.Printf("Worker %d : %+v\n", worker.Id, worker.Result)
+	}
 
 }
 
-func Sum(a, b int) {
-	fmt.Println(a + b)
+func Sum(a, b int) int {
+	return a + b
+}
+
+func Multiple(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("b must be different from 0")
+	}
+	return a / b, nil
 }
 
 func Delta(a, b int) {
 	fmt.Println(a - b)
+}
+
+func MultipleAndDivide(a, b int) (int, int, error) {
+	mul := Multiple(a, b)
+	div, err := Divide(a, b)
+	if err != nil {
+		return mul, 0, err
+	}
+
+	return mul, div, nil
 }
--- workerpool/pool.go
+++ workerpool/pool.go
@@ -3,6 +3,7 @@ package workerpool
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"sync"
 )
 
@@ -13,28 +14,35 @@ type WorkerPool struct {
 }
 
 type Worker struct {
-	Mutex *sync.Mutex
+	Id     int
+	Result map[string]interface{}
 }
 
 type Task struct {
-	Name string
-	Exec func()
+	Name    string
+	Execute func() interface{}
 }
 
 // Initialize a WorkerPool
 func Init(numOfWorkers int) *WorkerPool {
+	var workers []*Worker
+	for i := 1; i <= numOfWorkers; i++ {
+		workers = append(workers, &Worker{
+			Id: i,
+		})
+	}
 	return &WorkerPool{
-		Workers: make([]*Worker, numOfWorkers),
+		Workers: workers,
 		Pool:    make(chan *Task),
 		Wg:      &sync.WaitGroup{},
 	}
 }
 
 // Initialize a task with name and corresponding handler function
-func NewTask(name string, handler func()) *Task {
+func NewTask(name string, handler func() interface{}) *Task {
 	return &Task{
-		Name: name,
-		Exec: handler,
+		Name:    name,
+		Execute: handler,
 	}
 }
 
@@ -53,9 +61,12 @@ func (wm *WorkerPool) Start() {
 	for i := range wm.Workers {
 		go wm.Workers[i].run(wm.Pool, wm.Wg)
 	}
+
 }
 
 func (w *Worker) run(taskQueue chan *Task, wg *sync.WaitGroup) {
+	w.Result = make(map[string]interface{})
+
 	for task := range taskQueue {
 		func() {
 			defer func() {
@@ -63,22 +74,42 @@ func (w *Worker) run(taskQueue chan *Task, wg *sync.WaitGroup) {
 					fmt.Println("Recovered. Your task has a panic !")
 				}
 			}()
-			task.Exec()
+
+			result := task.Execute()
+
+			if result != nil {
+				if reflect.ValueOf(result).IsValid() {
+					w.Result[task.Name] = result
+				}
+			}
 		}()
 
 		wg.Done()
 	}
 }
 
-// Add n workers to current pool(if n < 0 then pool will reduced n workers)
+func (wm *WorkerPool) GetResult(taskName string) interface{} {
+	for i := range wm.Workers {
+		if _, ok := wm.Workers[i].Result[taskName]; ok {
+			return wm.Workers[i].Result[taskName]
+		}
+	}
+	return nil
+}
+
+// Add n workers to current pool(if n < 0 then pool will remove n last workers)
 func (wm *WorkerPool) AddWorker(n int) error {
+	lastWorkerId := len(wm.Workers)
 	if n >= 0 {
+		id := lastWorkerId
 		for i := 1; i <= n; i++ {
-			wm.Workers = append(wm.Workers, &Worker{})
+			id += 1
+			wm.Workers = append(wm.Workers, &Worker{
+				Id:     id,
+				Result: make(map[string]interface{}),
+			})
 		}
 	} else {
-		lastWorkerId := len(wm.Workers)
-
 		if n > lastWorkerId {
 			return errors.New("n must be less than total current number of workers of pool")
 		}
@@ -93,4 +124,17 @@ func (wm *WorkerPool) AddWorker(n int) error {
 // Get total current number of workers of pool
 func (wm *WorkerPool) TotalWorkers() int {
 	return len(wm.Workers)
+}
+
+// worker refreshing will free result stored at a list of workers (free all workers if calling function without params)
+func (wm *WorkerPool) RefreshWorker(id ...int) {
+	if len(id) == 0 {
+		for i := range wm.Workers {
+			wm.Workers[i].Result = nil
+		}
+	} else {
+		for i := 0; i <= len(id); i++ {
+			wm.Workers[i].Result = nil
+		}
+	}
 }
